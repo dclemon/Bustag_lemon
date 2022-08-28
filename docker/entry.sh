ce965cb69a825cc8b1d2a92343c3fda9e0eac870
dclemon <63597032+dclemon@users.noreply.github.com>
2022-08-28 19:38:10 +0800

    Add files via upload

---
+++ docker/entry.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PYTHON=python3
+# check if crontab.txt exists
+
+echo `pwd`
+
+${PYTHON} -m bustag.app.index
---
+++ docker/run_download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /app
+/usr/local/bin/python -m bustag.main download
+/usr/local/bin/python -m bustag.main recommend
